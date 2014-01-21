0bdd4d627bef14dc9468e2cb10242d5084d4b6d8
Lukas Ransmayr <lukas.ransmayr@gmail.com>
2014-01-20 20:33:52 -0600

    Playing with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Ags10::Application.routes.draw do
 
+  devise_for :users
+  root :to => 'static_pages#welcome'
+
   get "locations/index" => 'locations#index'
   get "/turniere" => 'locations#index'
 
