9953ef0e7cc617d9d2ae240a7f3c9b4b3eae184b
Lukas Ransmayr <lukas.ransmayr@gmail.com>
2014-01-27 19:45:07 -0600

    Changed gem File for early test production on heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,21 @@
 Ags10::Application.routes.draw do
 
-  devise_for :users
-  root :to => 'static_pages#home'
+  # devise_for :users
+  # root :to => 'static_pages#home'
 
-  get "welcome" => 'static_pages#welcome', as: 'welcome'
+  # get "welcome" => 'static_pages#welcome', as: 'welcome'
 
-  get "locations/index" => 'locations#index'
-  get "/turniere" => 'locations#index'
+  # get "locations/index" => 'locations#index'
+  # get "/turniere" => 'locations#index'
 
-  get "/locations/new" => 'locations#new'
-  post "/locations" => 'locations#create'
+  # get "/locations/new" => 'locations#new'
+  # post "/locations" => 'locations#create'
 
-  get "/anmeldung" => 'static_pages#anmeldung', as: 'anmeldung'
+  # get "/anmeldung" => 'static_pages#anmeldung', as: 'anmeldung'
+
+  # PRE PRODUCTION FEEDBACK ROUTE
+
+  root :to => 'static_pages#anmeldung'
 
 
 
